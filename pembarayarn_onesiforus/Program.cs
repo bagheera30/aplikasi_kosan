4447d9f08cafb243fc07ebeb571bd27e8d96ff37
Onesiforus-Rade-Putra <onesiforusradeputra@gmail.com>
2023-05-09 11:12:21 +0700

    Add project files.

---
+++ pembarayarn_onesiforus/Program.cs
@@ -0,0 +1,29 @@
+﻿using pembarayarn_onesiforus;
+
+class Program
+{
+    public static object ConfigurationManager { get; private set; }
+
+    static void Main(string[] args)
+    {
+        // Ambil pengaturan dari runtime configuration
+        double maxAmount = Convert.ToDouble(ConfigurationManager.["MaxAmount"]);
+        string pin = ConfigurationManager.["PIN"];
+
+        // Inisialisasi mesin pembayaran dengan pengaturan dari runtime configuration
+        PaymentMachine machine = new PaymentMachine(maxAmount, pin);
+
+        // Mulai mesin pembayaran
+        machine.Start();
+
+        // Loop untuk input
+        while (true)
+        {
+            Console.WriteLine("Silakan masukkan nomor transaksi:");
+            string input = Console.ReadLine();
+
+            // Kirim input ke mesin pembayaran
+            machine.ProcessInput(input);
+        }
+    }
+}
