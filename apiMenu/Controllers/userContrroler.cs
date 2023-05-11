4de6df4cbe2cdae46265d588f8ec0563551f5854
bagheera30 <rizkialfian30103@gmail.com>
2023-05-11 11:09:09 +0700

    benerin contoler

--- apiMenu/Controllers/userContrroler.cs
+++ apiMenu/Controllers/userContrroler.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using menu_pembelian;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -6,38 +7,40 @@ namespace apiMenu.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class userContrroler : ControllerBase
     {
+        private const string filePath = "users.json";
         // GET: api/<ValuesController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public ActionResult<List<users>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return usermanegement.GetUsers();
         }
 
         // GET api/<ValuesController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{nama}")]
+        public ActionResult<users> Get(string nama)
         {
-            return "value";
+            users u = usermanegement.getUsersbynama(nama);
+            if (u != null)
+            {
+                return u;
+            }
+            else
+            {
+                return NotFound();
+            }
         }
 
         // POST api/<ValuesController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult Post([FromBody] users value)
         {
+            usermanegement.addusers(value);
+            usermanegement.Serialize();
+            return CreatedAtAction(nameof(Get), null);
         }
 
-        // PUT api/<ValuesController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<ValuesController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        
     }
 }
